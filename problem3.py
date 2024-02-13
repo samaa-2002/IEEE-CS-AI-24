a6b433cb373f2aba94cbb7cfa6c229e6f674e481
samaa-2002 <132456121+samaa-2002@users.noreply.github.com>
2024-02-14 00:34:54 +0200

    Delete problem3.py

--- problem3.py
+++
@@ -1,7 +0,0 @@
-if __name__ == '__main__':
-    n = int(input())
-    arr = list(map(int, input().split()))  # convert map object to list
-    scores = set(list(arr))
-    scores.remove(max(scores))
-    running_up_score=max(scores)
-    print(running_up_score)
